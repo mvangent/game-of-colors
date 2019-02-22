e755ed519de8983c753e533462dfde33b696fcfa
Melvin van Gent <melvin@instantmagazine.com>
2019-02-22 21:54:11 +0100

    setup React + Typescript + webpack

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './App';
+// FIXME: decide upon sytem import './index.css';
+
+ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
